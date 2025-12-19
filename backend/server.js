73b2c3dc4c7fdc2e18240b65b25b897aa5a4b88d
asshh0x-crypto <asshh0x.com>
2025-12-19 18:39:18 +0530

    second 2.2

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,25 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import postsRoutes from "./routes/posts.routes.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // ⬅ always before routes
+app.use("/", postsRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Backend server is running successfully 🚀");
-});
+const start = async () => {
+  const connectDB = await mongoose.connect(
+    "mongodb+srv://eboouser:F7nmKPd4fiJEfK2i@getjobs.mwbb653.mongodb.net/?appName=getjobs"
+  );
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  app.listen(9080, () => {
+    console.log("🚀 Server is running on port 9080");
+  });
+};
+
+start();
